10db44064f98db2a32734f7af3f9fca6e3c7e03e
Chirav Dave <davechirav@gmail.com>
2019-03-22 23:37:23 -0700

    cosmetic changes

--- pingpong/src/test_computer_node.py
+++ pingpong/src/test_computer_node.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 import rospy
-from math import sqrt
 from numpy.random import choice
-from PingPong.msg import BallInfo, MovePlayerPaddle
+from PingPong.msg import BallInfo, MoveComputerPaddle
 from PingPong.srv import GameInfo
 from paddle import Paddle
 
-class PlayerNode:
+class ComputerNode:
 
 	def __init__(self):
-		self.action_publisher = rospy.Publisher('move_player_paddle', MovePlayerPaddle, queue_size=10)
+		self.action_publisher = rospy.Publisher('move_computer_paddle', MoveComputerPaddle, queue_size=10)
 		#Wait until the game node starts running
 		print 'Waiting to get game information'
 		rospy.wait_for_service('game_info')
@@ -21,37 +20,31 @@ class PlayerNode:
 			self.board_width = game_info.board_width
 			self.ball_speed = game_info.ball_speed
 			self.scale = game_info.scale
-			self.player = Paddle(self.board_width/2, self.board_length)
+			self.computer = Paddle(self.board_width/2, 0)
 			print 'Ready to win!'
 			#For listening ball movements
-			rospy.Subscriber("ball_info", BallInfo, self.move_player_paddle)
-			self.move_msg = MovePlayerPaddle()
+			rospy.Subscriber("ball_info", BallInfo, self.move_computer_paddle)
+			self.move_msg = MoveComputerPaddle()
 		except rospy.ServiceException, e:
 			print 'Service call failed: %s'%e
 
 	def publish_action(self, action):
 		'''
-		This method publishes the best action for the player
+		This method publishes the best action for the computer
 		'''
 		self.move_msg.where = action
 		print(action)
 		self.action_publisher.publish(self.move_msg)
 
-	def euclidean_distance(self, player_x, player_y, ball_x, ball_y):
-		'''
-		This method calculates l2 distance between the center of the player's paddle and center of the ball
-		'''
-		return sqrt(pow(player_x-ball_x, 2) + pow(player_y-ball_y, 2))
-
-	def move_player_paddle(self, data):
-		action = choice(['up', 'down'])
+	def move_computer_paddle(self, data):
+		action = choice(['stay', 'up', 'down'])
 		if action == 'up':
-			self.player.update_location(-self.scale)
+			self.computer.update_location(-self.scale) 
 		elif action == 'down':
-			self.player.update_location(self.scale)
+			self.computer.update_location(self.scale)
 		self.publish_action(action) 
 
 if __name__ == '__main__':
-	rospy.init_node('player_node', anonymous=True)
-	player = PlayerNode()
+	rospy.init_node('computer_node', anonymous=True)
+	computer = ComputerNode()
 	rospy.spin()
--- pingpong/src/test_player_node.py
+++ pingpong/src/test_player_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from math import sqrt
+from numpy.random import choice
 from PingPong.msg import BallInfo, MovePlayerPaddle
 from PingPong.srv import GameInfo
 from paddle import Paddle
@@ -24,6 +24,7 @@ class PlayerNode:
 			print 'Ready to win!'
 			#For listening ball movements
 			rospy.Subscriber("ball_info", BallInfo, self.move_player_paddle)
+			self.move_msg = MovePlayerPaddle()
 		except rospy.ServiceException, e:
 			print 'Service call failed: %s'%e
 
@@ -31,27 +32,12 @@ class PlayerNode:
 		'''
 		This method publishes the best action for the player
 		'''
-		self.action_publisher.publish(action)
-
-	def euclidean_distance(self, player_x, player_y, ball_x, ball_y):
-		'''
-		This method calculates l2 distance between the center of the player's paddle and center of the ball
-		'''
-		return sqrt(pow(player_x-ball_x, 2) + pow(player_y-ball_y, 2))
+		self.move_msg.where = action
+		print(action)
+		self.action_publisher.publish(self.move_msg)
 
 	def move_player_paddle(self, data):
-		ball_x, ball_y, direction = data.x, data.y, data.direction
-		action, score = 'stay', self.euclidean_distance(self.player.x, self.player.y, ball_x, ball_y)
-		#To check if up action can be performed
-		if self.player.x > 1.5*self.scale:
-			up_score = self.euclidean_distance(self.player.x-self.scale, self.player.y, ball_x, ball_y)
-			if up_score < score:
-				action = 'up'
-				score = up_score
-		if self.player.x < self.board_width-1.5*self.scale:
-			down_score = self.euclidean_distance(self.player.x+self.scale, self.player.y, ball_x, ball_y)
-			if down_score < score:
-				action = 'down'
+		action = choice(['stay', 'up', 'down'])
 		if action == 'up':
 			self.player.update_location(-self.scale)
 		elif action == 'down':
