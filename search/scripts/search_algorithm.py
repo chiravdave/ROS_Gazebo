0305d45b8d0c7dd30d06ffd6d8debfa29408064d
Chirav Dave <davechirav@gmail.com>
2019-03-26 01:52:07 -0700

    search updated

--- search/scripts/search_algorithm.py
+++ search/scripts/search_algorithm.py
@@ -6,90 +6,72 @@ import rospy
 from std_msgs.msg import String
 import argparse
 
-rospy.init_node('search_algorithms')
-publisher = rospy.Publisher('/actions',String,queue_size =10)
-parser = argparse.ArgumentParser()
-parser.add_argument('-a',help = 'Please mention algorithm to use. Default is BFS', metavar = 'bfs', action='store', dest='algorithm', default="bfs", type=str)
-
-
-
 def bfs():
-    init_state = problem.get_initial_state()
-    goal_state = problem.get_goal_state()
-    possible_actions = problem.get_actions() 
-    action_list = []
+	init_state = problem.get_initial_state()
+	goal_state = problem.get_goal_state()
+	possible_actions = problem.get_actions() 
+	action_list = []
+	#to get the next state, cost for an action on state_x use:
+	(nextstate, cost) = problem.get_successor(state, action)
+	'''
+	YOUR CODE HERE
+	'''
 
-    #to get the next state, cost for an action on state_x use:
-    (nextstate, cost) = problem.get_successor(state, action)
-
-    '''
-    YOUR CODE HERE
-    '''
-
-
-    return action_list
+	return action_list
 
 def ucs():
-    init_state = problem.get_initial_state()
-    goal_state = problem.get_goal_state()
-    possible_actions = problem.get_actions() 
-    action_list = []
+	init_state = problem.get_initial_state()
+	goal_state = problem.get_goal_state()
+	possible_actions = problem.get_actions() 
+	action_list = []
+	#to get the next state, cost for an action on state_x use:
+	(nextstate, cost) = problem.get_successor(state, action) ## This will return state with x and y co-ordinate -1 if action is not executable.
+	'''
+	YOUR CODE HERE
+	'''
 
-    #to get the next state, cost for an action on state_x use:
-    (nextstate, cost) = problem.get_successor(state, action) ## This will return state with x and y co-ordinate -1 if action is not executable.
-
-    '''
-    YOUR CODE HERE
-    '''
-
-
-    return action_list
+	return action_list
 
 def gbfs():
-    init_state = problem.get_initial_state()
-    goal_state = problem.get_goal_state()
-    possible_actions = problem.get_actions() 
-    action_list = []
+	init_state = problem.get_initial_state()
+	goal_state = problem.get_goal_state()
+	possible_actions = problem.get_actions() 
+	action_list = []
+	#to get the next state, cost for an action on state_x use:
+	(nextstate, cost) = problem.get_successor(state, action)
+	'''
+	YOUR CODE HERE
+	'''
 
-    #to get the next state, cost for an action on state_x use:
-    (nextstate, cost) = problem.get_successor(state, action)
-
-    '''
-    YOUR CODE HERE
-    '''
-
-
-    return action_list
+	return action_list
 
 def astar():
-    init_state = problem.get_initial_state()
-    goal_state = problem.get_goal_state()
-    possible_actions = problem.get_actions() 
-    action_list = []
+	init_state = problem.get_initial_state()
+	goal_state = problem.get_goal_state()
+	possible_actions = problem.get_actions() 
+	action_list = []
+	#to get the next state, cost for an action on state_x use:
+	(nextstate, cost) = problem.get_successor(state, action)
+	'''
+	YOUR CODE HERE
+	'''
 
-    #to get the next state, cost for an action on state_x use:
-    (nextstate, cost) = problem.get_successor(state, action)
+	return action_list
 
-    '''
-    YOUR CODE HERE
-    '''
-
-
-    return action_list
-
-
-   
-
- # to execute a plan action_list = <list of actions>, use:
+# to execute a plan action_list = <list of actions>, use:
 def exec_action_list(action_list):
-    plan_str = '_'.join(action for action in action_list)
-    publisher.publish(String(data = plan_str))
+	plan_str = '_'.join(action for action in action_list)
+	publisher.publish(String(data = plan_str))
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    algorithm = globals().get(args.algorithm)
-    if algorithm is None:
-        print 'Incorrect Algorithm name.'
-        exit(1)
-    actions = algorithm()
-    exec_action_list(actions)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-a',help = 'Please mention algorithm to use. Default is BFS', metavar = 'bfs', action='store', dest='algorithm', default="bfs", type=str)
+	args = parser.parse_args()
+	algorithm = globals().get(args.algorithm)
+	if algorithm is None:
+		print 'Incorrect Algorithm name.'
+		exit(1)
+	rospy.init_node('search_algorithms', anonymous = True)
+	publisher = rospy.Publisher('/actions',String,queue_size =10)
+	actions = algorithm()
+	exec_action_list(actions)
